3af840f166fb68c36131d7149f367720ec17a831
Eromosele <isiboreromoselejason@gmail.com>
2021-03-09 22:11:21 +0100

    html with javascript

---
+++ lesson.js
@@ -0,0 +1,30 @@
+export const lessons = [
+    {
+        subject: 'Math',
+        location: 'London',
+        price: '£100',
+        spaces: 5,
+        image: 'https://www.fife.ac.uk/media/3828/mathematics-png.jpg?anchor=center&mode=crop&width=1200&height=800&rnd=132155248940000000'
+    },
+    {
+        subject: 'Math',
+        location: 'Oxford',
+        price: '£100',
+        spaces: 5,
+        image: 'https://jobs.newscientist.com/getasset/c40a5488-11be-43b0-843f-a2e6ef9f0612/'
+    },
+    {
+        subject: 'English',
+        location: 'York',
+        price: '£80',
+        spaces: 5,
+        image: 'https://www.environmentalscience.org/wp-content/uploads/2018/08/physics-640x416.jpg'
+    },
+    {
+        subject: 'Music',
+        location: 'Bristol',
+        price: '£90',
+        spaces: 5,
+        image: 'https://buid.ac.ae/wp-content/uploads/2020/09/management-accounting-header.jpg'
+    }
+  ]

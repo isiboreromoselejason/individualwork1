40ee1013d2e5d77260ae938764f3d0367949ac08
Eromosele <isiboreromoselejason@gmail.com>
2021-03-10 09:32:16 +0100

    js changes

--- lessons.js
+++ lessons.js
@@ -7,23 +7,23 @@ export const lessons = [
         image: 'https://www.fife.ac.uk/media/3828/mathematics-png.jpg?anchor=center&mode=crop&width=1200&height=800&rnd=132155248940000000'
     },
     {
-        subject: 'Chemistry',
-        location: 'Accra',
-        price: '£70',
+        subject: 'Math',
+        location: 'Oxford',
+        price: '£100',
         spaces: 5,
         image: 'https://jobs.newscientist.com/getasset/c40a5488-11be-43b0-843f-a2e6ef9f0612/'
     },
     {
-        subject: 'Physics',
-        location: 'New York',
-        price: '£130',
+        subject: 'English',
+        location: 'London',
+        price: '£100',
         spaces: 5,
         image: 'https://www.environmentalscience.org/wp-content/uploads/2018/08/physics-640x416.jpg'
     },
     {
-        subject: 'Accounting',
-        location: 'Paris',
-        price: '£60',
+        subject: 'English',
+        location: 'York',
+        price: '£90',
         spaces: 5,
         image: 'https://buid.ac.ae/wp-content/uploads/2020/09/management-accounting-header.jpg'
     }
